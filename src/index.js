15fb18188728df17a83f0df7d74429eb897e7c57
ALP300 <aitorpalomino1@gmail.com>
2026-04-05 19:28:21 -0500

    A

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import "dotenv/config";
 import express from "express";
 import { conectar } from "./config/database.js";
 
+import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 import setupSwagger from "./config/swagger.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 conectar();
